131979e2410978f9d52fd0be515da240ef3590b8
zachrey <zz__0123@163.com>
2017-06-27 21:19:12 +0800

    初始化前端路由

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {Router, Route, browserHistory} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routers = () => (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+);
+
+ReactDOM.render(<Routers />, document.getElementById('root'));
 registerServiceWorker();
