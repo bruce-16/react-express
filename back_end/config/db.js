e8bbde985f7b3f86543b56dbc26d319a9c0caf57
zachrey <zz__0123@163.com>
2017-07-16 11:53:01 +0800

    增加服务成功监听，数据库链接成功和异常监听

---
+++ back_end/config/db.js
@@ -0,0 +1,17 @@
+//返回mongoose连接实例
+var mongoose = require('mongoose');
+var cfg = require('./mongodb');
+console.log(cfg.mongodbUrl);
+mongoose.connect(cfg.mongodbUrl, {useMongoClient: true});
+
+//链接成功的监听
+mongoose.connection.on('connected', () => {    
+    console.log('Mongoose connection open to ' + cfg.mongodbUrl);  
+});
+
+//链接失败的监听
+mongoose.connection.on('error', err => {    
+    console.log('Mongoose connection error: ' + err);  
+});
+
+module.exports = mongoose;
