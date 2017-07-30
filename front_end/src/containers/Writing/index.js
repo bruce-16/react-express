3f9d7aad14f8d8fb9a1740d5b50d612858f43f3d
zachrey <zz__0123@163.com>
2017-07-30 21:26:55 +0800

    表单提交修改

--- front_end/src/containers/Writing/index.js
+++ front_end/src/containers/Writing/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
-import { Input, Row, Button } from 'antd';
+import { Input, Row, Button, Form, Modal } from 'antd';
+import {USER, getItem} from '../../utils/localStorage';
+import {addRecord} from '../../server/records';
+
+const FormItem = Form.Item; 
 
  const Title = (
   <span style={{fontSize: 15}}>标题：</span>
@@ -11,37 +15,68 @@ class Writing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      content: '',
       loading: false
     }
   }
 
-  handleChange = (value) => {
-    this.setState({ text: value })
+  handleContentChange = (value) => {
+    this.setState({ content: value });
   }
 
-  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({loading: true});
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+      // 提交加入内容和作者ID,
+      values.content = this.state.content;
+      values.author = JSON.parse(getItem(USER))._id;
+      addRecord(values, (rst) => {
+        Modal.success({title:'提示', content: '发表成功！'});
+        this.setState({loading: false});
+      });
+    });
+  }
 
   render (){
+    const {getFieldDecorator} = this.props.form;
     return (
       <div style={{marginTop: 20}}>
-        <Input addonBefore={Title} style={{width: '100%'}} size="large"/>
-        <ReactQuill value={this.state.text}
-          onChange={this.handleChange}>
-          <Input type="textarea" rows="20"/>
-        </ReactQuill>
-        <Row type="flex" justify='center' style={{margin: '20px 0'}}>
-          <Button type="" style={{marginRight: '20px'}} onClick={() => {
-            console.log('点击取消');
-          }}>取消</Button>
-          <Button loading={this.state.loading} type="primary" onClick={() => {
-            this.setState({loading: true});
-            console.log('点击提交');
-          }}>确定</Button>
-        </Row>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem>
+            {getFieldDecorator('title', {
+              rules: [{ 
+                required: true, 
+                message: '请输入标题' 
+              }],
+            })(
+              <Input 
+                addonBefore={Title} 
+                style={{width: '100%'}} 
+                size="large"/>
+            )}
+          </FormItem>
+          {/* 内容区域  */}
+          <ReactQuill value={this.state.content}
+            onChange={this.handleContentChange}>
+            <div className="my-editing-area" style={{height: 400}}/>
+          </ReactQuill>
+          {/* 按钮区域 */}
+          <FormItem>
+            <Row type="flex" justify='center' style={{margin: '20px 0'}}>
+              <Button type="" style={{marginRight: '20px'}} onClick={() => {
+                console.log('点击取消');
+              }}>取消</Button>
+              <Button loading={this.state.loading} type="primary" htmlType="submit">确定</Button>
+            </Row>
+          </FormItem>
+        </Form>
       </div>
     );
   }
 }
-
-export default Writing;
+const WritingForm = Form.create()(Writing);
+export default WritingForm;
